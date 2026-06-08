7dd5acd5ff2ee4086f06044378e3afbca4fbee3a
GAABR <43247478+GAABR@users.noreply.github.com>
2026-06-08 05:56:41 -0400

    Add files via upload

--- listes.js
+++ listes.js
@@ -15,8 +15,10 @@ window.LISTES = {
 ],
 
   mots: [
-    "volé", "joli", "olive", "avalé", "vélo", "rire",
-    "lire", "il a mal", "favori", "affamé", "affolé"
+    "il se lève", "sur", "même", "forê\\t", "il lave", "rare", "mêlé", "fumé\\e", "levé", "rire", "larme", "fossé", "sale", "ravi", "armé\\e", "farfelu", "sali", "lire", "rime", "foli\\e", "salé", "salir", "rame", "affolé", "volé", "rôle", "mûrir", "favori", "allé", "la ru\\e", "allumé", "affamé", "joli", "la revu\\e", "le mur", "la fé\\e", "olive", "la rivière", "la mi\\e", "le fil", "salué", "il rira", "le mari", "la file", "il a avalé", "il lira", "ma lime", "la fève", "le sol", "il volera", "la mule", "le film", "la salle", "il salera", "le mime", "je filme", "le vélo", "il avalera", "la momi\\e", "il filme", "la vallée", "il se lavera", "ma mère", "for\\t", "il a vu", "il a réussi", "le lama", "la forme", "il a lu", "il arrive", "la masse", "le fémur", "il a su", "il rêve", "la somme", "la formule", "le li\\t", "siro\\p", "il a mal", "il a réussi", "le lila\\s", "il a ramassé", "il a filmé", "si\\x", "la mûre"
+
+
+
   ]
 
 };
